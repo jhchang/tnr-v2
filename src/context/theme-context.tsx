664146dbfab059f390f73d57b5e2817e2d6479e5
Justin Chang <justinhchang743@gmail.com>
2024-10-12 17:05:09 -0700

    added background colors, app icon, and theme-switcher

---
+++ src/context/theme-context.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import React, { useEffect, useState, createContext, useContext } from 'react';
+
+type Theme = 'light' | 'dark';
+
+type ThemeContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type ThemeContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+export default function ThemeContextProvider({
+  children,
+}: ThemeContextProviderProps) {
+  const [theme, setTheme] = useState<Theme>('light');
+
+  // set user's local storage with updated settings
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+      window.localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    } else {
+      setTheme('light');
+      window.localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove('dark');
+    }
+  };
+
+  useEffect(() => {
+    // check user's local storage from last visit to see what they set the theme to
+    const localTheme = window.localStorage.getItem('theme') as Theme | null;
+
+    // if user has not visited before then check OS preference
+    if (localTheme) {
+      setTheme(localTheme);
+
+      if (localTheme === 'dark') {
+        document.documentElement.classList.add('dark');
+      }
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      // upon first visit check OS theme preference
+      setTheme('dark');
+      document.documentElement.classList.add('dark');
+    }
+  }, []);
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (context === null) {
+    throw new Error('useTheme must be used within a ThemeContextProvider');
+  }
+
+  return context;
+}
