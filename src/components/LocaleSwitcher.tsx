a2508ec8ca1fa85c98c99582cfb20640da01dd5c
Justin Chang <justinhchang743@gmail.com>
2025-05-09 14:40:50 -0700

    made locale switcher absolute position so it doesn't scroll and added inline custom breakpoint so it header doesn't cover it on small screens

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,16 +1,13 @@
-'use client';
-
 import { useLocale, useTranslations } from 'next-intl';
 import { routing } from '@/i18n/routing';
 import LocaleSwitcherSelect from './LocaleSwitcherSelect';
-import { motion } from 'framer-motion';
 
 export default function LocaleSwitcher() {
   const t = useTranslations('LocaleSwitcher');
   const locale = useLocale();
 
   return (
-    <motion.div className='fixed top-[0.15rem] right-7 bg-white bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-md flex items-center justify-center sm:top-6 hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950'>
+    <div className='absolute top-[80] right-7 bg-white bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-md flex items-center justify-center [@media(min-width:890px)]:top-6 hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950'>
       <LocaleSwitcherSelect defaultValue={locale} label={t('label')}>
         {routing.locales.map((cur) => (
           <option key={cur} value={cur}>
@@ -18,6 +15,6 @@ export default function LocaleSwitcher() {
           </option>
         ))}
       </LocaleSwitcherSelect>
-    </motion.div>
+    </div>
   );
 }
