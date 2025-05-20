b8ec22143460a0340569bab434500c6d30888414
Justin Chang <justinhchang743@gmail.com>
2025-05-19 17:44:13 -0700

    added hover effect on hamburger menu items

--- src/components/mobile-header.tsx
+++ src/components/mobile-header.tsx
@@ -34,7 +34,7 @@ export default function MobileHeader() {
             <SheetDescription>
               {links.map((link) => (
                 <Link
-                  className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300'
+                  className='flex w-full items-center justify-center px-3 py-3 text-gray-500 hover:text-gray-950 hover:bg-gray-100 transition dark:text-gray-500 dark:hover:text-gray-300 dark:hover:bg-gray-800 rounded-full'
                   href={link.hash}
                   key={link.hash}
                 >
